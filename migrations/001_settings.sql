fa87d23b63ea2068b4fdb8fe80ba8a2d9cc2bdcb
Zdeněk Papučík <zdenek.papucik@gmail.com>
2026-06-09 13:54:58 +0200

    dev

--- migrations/001_settings.sql
+++ migrations/001_settings.sql
@@ -3,7 +3,8 @@
 -- -------------
 CREATE TABLE `settings` (
     `name` varchar(100) NOT NULL,
-    `value` varchar(255) NOT NULL
+    `value` varchar(255) NOT NULL,
+    `description` varchar(255) NOT NULL
 )
     ENGINE=InnoDB
     DEFAULT CHARSET=utf8;
